e02e614a5d672c95f0f5d2252870dedb5eea920e
Janani Devarajan <139041316+JananiDeva@users.noreply.github.com>
2024-07-19 11:44:22 +0530

    Add files via upload

---
+++ jd.js
@@ -0,0 +1,9 @@
+function fun(){
+    var a=document.getElementById("text")
+    a.style.color="red"
+}
+function func1(){
+    var a1=document.getElementById("ph")
+    a1.src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRAgjeh_PZSC9gUmlm1dzEkbCsP71owoucsFWhoIYYpp7u1czrj"
+    alert("Your Image Is Successfully Changed")
+}
